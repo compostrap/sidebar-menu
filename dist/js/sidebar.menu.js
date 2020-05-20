3e60a18b436448d6246f131fb6a44a51d64a2818
accgit <zdenek.papucik@gmail.com>
2020-05-20 09:24:22 +0200

    Update

--- dist/js/sidebar.menu.js
+++ dist/js/sidebar.menu.js
@@ -6,50 +6,49 @@
 (function($) {
 
   // class with arrow icon
-  var linkArrow     = '.link-arrow';
+  const linkArrow = '.link-arrow';
 
   // class current link
-  var linkCurrent   = '.link-current';
+  const linkCurrent = '.link-current';
 
   // class hidden list
-  var listHidden    = '.list-hidden';
+  const listHidden = '.list-hidden';
 
   // class list item
-  var listItem      = '.list-item';
+  const listItem = '.list-item';
 
   // list init
   $(listItem).each(function() {
 
-    var el = $(this);
-    var parent  = el.parent();
-
-    // current link class
-    var current = parent.find(linkArrow + linkCurrent);
+    const el = $(this);
+    const parent = el.parent();
+    const link = parent.find(linkArrow + linkCurrent);
 
     // default icon menu
     parent.find(linkArrow).addClass('up');
 
     // view the list above the current link
-    if (current.length > 0) {
+    if (link.length > 0) {
 
       // active down icon
-      current.addClass('active down');
+      link.addClass('active down');
 
       // show hidden list
-      current.next(listHidden).show();
+      link.next(listHidden).show();
     }
   });
 
   // list transition arrow
   $(linkArrow).on('click', function() {
 
-    var el = $(this);
+    const el = $(this);
+    const linkCurrent = 'link-current';
 
     // adding rotation effect to arrows icons
     el.addClass('transition').toggleClass('active rotate');
 
     // adding link current on click link
-    !(el.hasClass('link-current')) ? el.addClass('link-current') : el.removeClass('link-current');
+    !(el.hasClass(linkCurrent)) ? el.addClass(linkCurrent) : el.removeClass(linkCurrent);
 
     // show hidden list
     el.next(listHidden).slideToggle('fast');
